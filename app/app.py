ff56dca047bc5ac16185422aa10d157441a42d42
Jesus Maria Herruzo Luque <jhl1001@alu.ubu.es>
2020-06-05 00:00:42 +0200

    Mejoras visuales de la web #34

--- app/app.py
+++ app/app.py
@@ -14,22 +14,28 @@ from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 from matplotlib.figure import Figure
 from scipy import fft
 
-from modelo_perceptron import *
-from modelo_lstm import *
-from config import *
-from forms import *
+from .modelo_perceptron import *
+from .modelo_lstm import *
+from .config import *
+from .forms import *
+os.environ['KMP_DUPLICATE_LIB_OK']='True'
 
 app = Flask(__name__)
 babel = Babel(app)
 bootstrap = Bootstrap(app)
 app.config.from_object(Config)
-app.secret_key = app.config['MODEL_NAME']
+app.secret_key = app.config['MODEL_FOLDER']
 
 # Cargamos el modelo
-model = ModeloLSTM()
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')   
-model.load_state_dict(torch.load(app.config['MODEL_NAME'], map_location = device))
-model.eval()
+
+modelLSTM = ModeloLSTM()
+modelLSTM.load_state_dict(torch.load(app.config['MODEL_FOLDER'] + app.config['MODEL_NAMES'][0], map_location = device))
+modelLSTM.eval()
+
+modelPerceptron = Perceptron()
+modelPerceptron.load_state_dict(torch.load(app.config['MODEL_FOLDER'] + app.config['MODEL_NAMES'][1], map_location = device))
+modelPerceptron.eval()
 
 # Creamos el directorio para subir los ficheros
 if not os.path.exists(app.config['UPLOAD_FOLDER']):
@@ -64,6 +70,9 @@ def set_language(language=None):
 @app.route('/', methods=['POST'])
 def predict():  
     if request.method == 'POST':
+        form = UploadFileForm(request.form)
+        model = form.select.data        
+
         if 'file' not in request.files:
             flash('_(Archivo no encontrado en la petición)', 'danger')                        
             return redirect(request.url)
@@ -77,7 +86,7 @@ def predict():
             filename = secure_filename(file.filename)
             filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
             file.save(filepath)
-            df, predictions = inference(filepath)
+            df, predictions = inference(filepath, model)
             labels = [_('Estrellas'), _('Exoplanetas Detectados')]
             hovercolors = [ "rgb(199, 5, 5)", "rgb(0, 200, 234)" ]
             colors = [ "rgb(150, 4, 4)", "rgb(0, 154, 174)" ]
@@ -90,11 +99,14 @@ def predict():
                 'indexes': exoplanets_indexes
             }
             images = plot_flux(df, exoplanets_indexes)
-            return render_template('results.html', results = results, images = images,
+            return render_template('results.html', results = results, images = images, 
                                    values = values, labels = labels, colors = colors, hovercolors = hovercolors)
         else:
             flash(_('Solo se permiten archivos con extensión csv'), 'danger')
             return redirect(request.url)        
+
+def z_score_normalizing(df):
+    return (df - df.mean()) / df.std()  
 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in app.config['ALLOWED_EXTENSIONS']
@@ -135,12 +147,19 @@ def create_image(points, i, time_points, x_text):
 
     return pngImageB64String
 
-def inference(filepath):
+def inference(filepath, selected_model):
     df = pd.read_csv(filepath, low_memory=False)
     if df.shape[1] == 3198:
         df.drop(df.columns[0], axis = 1, inplace = True)
+    df = df.apply(z_score_normalizing, axis = 1)
     df_tensor = torch.tensor(df.values).float()
-    predictions = torch.argmax(model(df_tensor.view(len(df_tensor), 1, -1)), 1)
+    if selected_model == 'LSTM':
+        model = modelLSTM
+        tensor = df_tensor.view(len(df_tensor), 1, -1)
+    else:
+        model = modelPerceptron
+        tensor = df_tensor
+    predictions = torch.argmax(model(tensor), 1)
     return df, predictions
 
 @babel.localeselector
